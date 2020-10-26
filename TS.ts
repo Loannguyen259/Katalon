0b9ecca9f2e5bcc09956c15280b47dfbd3eb826c
Loannguyen259 <63132204+Loannguyen259@users.noreply.github.com>
2020-10-26 23:39:55 +0700

    Add files via upload

---
+++ TS.ts
@@ -0,0 +1,77 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>80a90eeb-3662-421e-88f9-f31b3a0765d7</testSuiteGuid>
+   <testCaseLink>
+      <guid>dd6dda3c-df18-4ac9-9961-d1e3ad28fc02</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DangNhap</testCaseId>
+      <testDataLink>
+         <combinationType>MANY</combinationType>
+         <id>1ee7fb96-3d76-48c8-93b0-a68b35ce06ba</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/FileData</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>1ee7fb96-3d76-48c8-93b0-a68b35ce06ba</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Tên đăng nhập</value>
+         <variableId>e0fee7e0-c2c2-4b76-9da9-9dbc00920aa7</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>1ee7fb96-3d76-48c8-93b0-a68b35ce06ba</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Mật khẩu</value>
+         <variableId>5d61e61d-6295-4594-8b05-2bfef5fca2a4</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c3db5d5b-20cf-4e1d-8fe2-f1d9bcd129ae</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Hoidap</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>393ad5de-e5ce-4867-a2e9-cb61e34a2bcd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Thongbao</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>436f2203-c851-4ace-aaf5-6ad197459440</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Chuongtrinhdaotao</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>cd6aa5b9-f7ff-44fe-8305-e9a8b166484d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Dangkyhocphan</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>651250fc-8546-40bd-bf70-be0461877f4c</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>0dffac59-6f8c-4e8f-9e94-3baa36b1317d</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
